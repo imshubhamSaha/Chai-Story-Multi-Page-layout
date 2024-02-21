055c0b5d11dbee24b9367dfe72bb508ff3906aef
Shubham Saha <imshubhamsaha@gmail.com>
2024-02-21 22:06:47 +0530

    feat/ added navbar

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,11 @@
 "use strict";
 
 const footerYear = document.getElementById("year");
+const navBtn = document.getElementById("nav-btn");
+const navbar = document.getElementById("navbar");
+const navClose = document.getElementById("nav-close");
+
 footerYear.innerHTML = new Date().getFullYear();
+
+navBtn.addEventListener("click", () => navbar.classList.add("show-nav"));
+navClose.addEventListener("click", () => navbar.classList.remove("show-nav"));
