bdcd450ab3da20c106b318500b2525bad39ff5cb
Shubham Saha <imshubhamsaha@gmail.com>
2024-03-20 22:31:58 +0530

    CHAI-STORY-PAGE/feat- Intergrate back to top button in footer with smooth scrolling

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,40 @@
-"use strict";
+'use strict';
 
-const footerYear = document.getElementById("year");
-const navBtn = document.getElementById("nav-btn");
-const navbar = document.getElementById("navbar");
-const navClose = document.getElementById("nav-close");
-var btn = $("#button");
+const footerYear = document.querySelector('#year');
+const navBtn = document.querySelector('#nav-btn');
+const navbar = document.querySelector('#navbar');
+const navClose = document.querySelector('#nav-close');
+const toTopBtn = document.querySelector('#button');
+
+/*
+*****************************************************************************************************************************************
+EVENT HANDLER FUNCTION
+*****************************************************************************************************************************************
+ */
+const scroll = () =>
+  document.body.scrollTop > 300 || document.documentElement.scrollTop > 300
+    ? toTopBtn.classList.add('show')
+    : toTopBtn.classList.remove('show');
+
+const topFunction = () => window.scrollTo({ top: 0, behavior: 'smooth' });
+/*
+*****************************************************************************************************************************************
+END EVENT HANDLER FUNCTION
+*****************************************************************************************************************************************
+ */
+/*
+*****************************************************************************************************************************************
+EVENTS
+*****************************************************************************************************************************************
+ */
+
 footerYear.innerHTML = new Date().getFullYear();
-
-navBtn.addEventListener("click", () => navbar.classList.add("show-nav"));
-navClose.addEventListener("click", () => navbar.classList.remove("show-nav"));
-
-window.onscroll = () => {
-  toggleTopButton();
-};
-function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-}
-
-function toggleTopButton() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("back-to-up").classList.remove("d-none");
-  } else {
-    document.getElementById("back-to-up").classList.add("d-none");
-  }
-}
+navBtn.addEventListener('click', () => navbar.classList.add('show-nav'));
+navClose.addEventListener('click', () => navbar.classList.remove('show-nav'));
+window.addEventListener('scroll', scroll);
+toTopBtn.addEventListener('click', topFunction);
+/*
+*****************************************************************************************************************************************
+END OF EVENTS 
+*****************************************************************************************************************************************
+ */
